733facd9396b78ab38ffea5a1e9d4537f765e8a4
Elyse <ehill@ualberta.ca>
2016-03-03 14:27:20 -0700

    UI WIP

---
+++ local/demo/lang/en/local_demo.php
@@ -0,0 +1,52 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'demo', language 'en'
+ *
+ * @package     local
+ * @subpackage  demo
+ * @copyright   Elyse Hill ehill@ualberta.ca
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+//$string['menuoption'] = 'Demo Plug-in';
+$string['pluginname']     = 'CMPUT 401 Demo Plug-in';
+$string['classselection'] = 'Please select a class section.';
+$string['genmakeheader']  = "General";
+$string['file']           = 'Upload';
+
+//Make Exams
+$string['createheader']   = 'Creating a new Exam';
+$string['makeexam']       = 'Name of Exam to Generate';
+$string['copyheader']     = 'Copy Options';
+$string['examcopies']     = 'How many booklets would you like generated?';
+$string['downloadheader'] = 'Download Options';
+$string['howtogenerate']  = 'How would you like to download your exam?';
+$string['multicopy']      = 'Individual Exam Files';
+$string['largecopy']      = 'One Large Exam File';
+$string['emergencypgs']   = 'How many blank pages would you like to include?';
+$string['emergencyslct']  = 'How many?'
+$string['generatebutton'] = 'Generate Exams';
+
+//Create Exam
+$string['markheader']     = 'Mark Exams';
+$string['sectionheader']  = 'Select course section(s)';
+$string['exambatch']      = 'Select Exam(s)';
+$string['markbutton']     = 'Mark Exams';
+
+?>
