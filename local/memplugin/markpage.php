eb59ed23820a2092a0f1018e8a3c63c18885a04d
jlx339 <jlx339@hotmail.com>
2016-03-04 14:09:07 -0700

    make page init

--- local/memplugin/markpage.php
+++ local/memplugin/markpage.php
@@ -1,15 +1,25 @@
 <?php
 global $PAGE, $CFG, $DB;
-require_once('../../config.php');
+require_once(dirname(__FILE__).'/../../../../config.php');
+
+require_once($CFG->dirroot.'/local/memplugin/qrtest_class.php');
+require_once($CFG->dirroot.'/local/memplugin/marklib.php');
+
+// parameter required
+$id = required_param('id', PARAM_INT);
+$submissionid = optional_param('submissionid', 0, PARAM_INT);
+
+$url = new moodle_url('/local/memplugin/markpage.php', array('submissionid'=>$submissionid,
+                                                                       'id' => $id));
+$PAGE->set_url($url);
 
 require_login();
-require_capability('local/memplugin:add', context_system::instance());
-require_once($CFG->dirroot.'/local/memplugin/qrtest_class.php');
+require_capability('local/memplugin:add', context_system::instance());//??
+
 $PAGE->set_context(context_system::instance());
 $PAGE->set_pagelayout('standard');
 $PAGE->set_title(get_string('pluginname', 'local_memplugin'));
 $PAGE->set_heading(get_string('pluginname', 'local_memplugin'));
-$PAGE->set_url($CFG->wwwroot.'/local/memplugin/markpage.php');
 
 
 echo $OUTPUT->header();
@@ -25,3 +35,37 @@ if (isset($_GET['page'])) {
 echo $OUTPUT->footer();
 
 ?>
+
+
+<?php
+
+
+//if (!is_null($rownum)) {
+//    $url->param('rownum', $rownum);
+//}
+//if (!is_null($returnparams)) {
+//    $url->param('returnparams', $returnparams);
+//}
+//$cm = get_coursemodule_from_id('assign', $id, 0, false, MUST_EXIST);
+//$course = $DB->get_record('course', array('id' => $cm->course), '*', MUST_EXIST);
+
+//$PAGE->set_url($url);
+//require_login($course, false, $cm);
+
+//$context = context_module::instance($cm->id);
+//require_capability('mod/assign:grade', $context);
+
+$assignment = new assign($context, $cm, $course);
+$feedbackpdf = new assign_feedback_pdf($assignment, 'feedback_pdf');
+
+if ($action == 'showprevious') {
+    $feedbackpdf->show_previous_comments($submissionid);
+} else if ($action == 'showpreviouspage') {
+    $feedbackpdf->edit_comment_page($submissionid, $pageno, false);
+} else if ($action == 'clearcache') {
+    $feedbackpdf->clear_image_cache($submissionid, optional_param('nextaction', null, PARAM_ALPHA));
+} else if ($action == 'browseimages') {
+    $feedbackpdf->browse_images($submissionid, $pageno);
+} else {
+    $feedbackpdf->edit_comment_page($submissionid, $pageno);
+}
