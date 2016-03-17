f73deb7752d0aa171520ebe40caa131590b817d6
jlx339 <jlx339@hotmail.com>
2016-03-17 00:52:21 -0600

    worked?

--- local/memplugin/markpage.php
+++ local/memplugin/markpage.php
@@ -1,66 +1,54 @@
 <?php
-defined('MOODLE_INTERNAL') || die('unable to connect');
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-global $PAGE, $CFG, $DB;
+/**
+ * The entry point for annotating a PDF
+ *
+ * @package   mod_assign
+ * @subpackage assignsubmission_pdf
+ * @copyright 2012 Davo Smith
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 require_once(dirname(__FILE__).'/../../../../config.php');
+global $CFG, $DB, $PAGE;
+require_once($CFG->dirroot.'/mod/assign/locallib.php');
+require_once($CFG->dirroot.'/mod/assign/submission/pdf/lib.php');
 
-<<<<<<< HEAD
-require_once($CFG->dirroot.'/local/memplugin/qrtest_class.php');
-require_once($CFG->dirroot.'/local/memplugin/marklib.php');
-
-// parameter required
-$id = required_param('id', PARAM_INT);
+$id   = required_param('id', PARAM_INT);
 $submissionid = optional_param('submissionid', 0, PARAM_INT);
+$pageno = optional_param('pageno', 1, PARAM_INT);
+$action = optional_param('action', null, PARAM_TEXT);
+$rownum = optional_param('rownum', null, PARAM_INT);
+$returnparams = optional_param('returnparams', null, PARAM_TEXT);
 
-$url = new moodle_url('/local/memplugin/markpage.php', array('submissionid'=>$submissionid,
-                                                                       'id' => $id));
+$url = new moodle_url('moodle/local/memplugin/markpage.php');
+if (!is_null($rownum)) {
+    $url->param('rownum', $rownum);
+}
+if (!is_null($returnparams)) {
+    $url->param('returnparams', $returnparams);
+}
+$cm = get_coursemodule_from_id('assign', $id, 0, false, MUST_EXIST);
+$course = $DB->get_record('course', array('id' => $cm->course), '*', MUST_EXIST);
+
 $PAGE->set_url($url);
-
 require_login();
-require_capability('local/memplugin:add', context_system::instance());//??
-=======
-require_login();
-require_capability('local/memplugin:add', context_system::instance());
->>>>>>> f741947de094d2cedccfad6e6f5a2e5c687bb116
 
-$PAGE->set_context(context_system::instance());
-$PAGE->set_pagelayout('standard');
-$PAGE->set_title(get_string('pluginname', 'local_memplugin'));
-$PAGE->set_heading(get_string('pluginname', 'local_memplugin'));
-
-
-echo $OUTPUT->header();
-
-if (isset($_GET['booklet'])) {
-	echo 'booklet: '.$_GET['booklet'];
-	echo '<br>';
-}
-if (isset($_GET['page'])) {
-	echo 'page: '.$_GET['page'];
-}
-
-echo $OUTPUT->footer();
-
-?>
-
-
-<?php
-
-
-//if (!is_null($rownum)) {
-//    $url->param('rownum', $rownum);
-//}
-//if (!is_null($returnparams)) {
-//    $url->param('returnparams', $returnparams);
-//}
-//$cm = get_coursemodule_from_id('assign', $id, 0, false, MUST_EXIST);
-//$course = $DB->get_record('course', array('id' => $cm->course), '*', MUST_EXIST);
-
-//$PAGE->set_url($url);
-//require_login($course, false, $cm);
-
-//$context = context_module::instance($cm->id);
-//require_capability('mod/assign:grade', $context);
+$context = context_module::instance($cm->id);
 
 $assignment = new assign($context, $cm, $course);
 $feedbackpdf = new assign_feedback_pdf($assignment, 'feedback_pdf');
