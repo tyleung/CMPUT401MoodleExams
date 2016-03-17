7aeb6b444cd2c2d3106787c50d6bc81b09034781
Prim <jaeyoon1@ualberta.ca>
2016-03-16 23:27:51 -0600

    barebone unit test..?

---
+++ local/memplugin/tests/mme_exams_test.php
@@ -0,0 +1,9 @@
+<?php
+require_once("../mme_exams.php");
+
+class local_memplugin_mme_exams_testcase extends basic_testcase {
+     public function test_adding() {
+         $this->assertEquals(2, 1+2);
+     }
+ }
+ ?>
