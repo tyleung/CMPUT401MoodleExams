d4c5afc2ab9a1fcb7d97bd14508ed1c324f1e944
Anon <jaeyoon1@ualberta.ca>
2016-03-18 08:25:43 -0600

    Fix up unit tests

--- local/memplugin/tests/mme_exams_test.php
+++ local/memplugin/tests/mme_exams_test.php
@@ -6,15 +6,17 @@ require_once($CFG->dirroot.'/local/memplugin/mme_exams.php');
 // Can't test exam generation atm as we cannot save the resulting pdf to file.
 class local_memplugin_mme_exams_testcase extends basic_testcase {
     public function test_get_QRcode_string() {
-        $exam = new MME_exams("test_sample.pdf","test_exam");
-        $this->assertEquals($exams->get_Qrcode_string(1,1),"test_exam:#1 pg:1"); //normal case
-        $this->assertEquals($exams->get_Qrcode_string(0,0),"test_exam:#0 pg:0");
+        global $CFG;
+        $exam = new MME_exams($CFG->dirroot.'/local/memplugin/fnl2012.pdf',"test_exam");
+        $this->assertEquals($exam->get_Qrcode_string(1,1),"test_exam:#1 pg:1"); //normal case
+        $this->assertEquals($exam->get_Qrcode_string(0,0),"test_exam:#0 pg:0");
     }
 
     public function test_get_QRcode_data() {
-    	$exam = new MME_exams("test_sample.pdf","test_exam");
+        global $CFG;
+    	$exam = new MME_exams($CFG->dirroot.'/local/memplugin/fnl2012.pdf',"test_exam");
     	$data = serialize(array('page_number'=>1,'exam_number'=>1,'name'=>"test_exam"));
-    	$this->assertEquals($exams->get_Qrcode_data(1,1),$data);
+    	$this->assertEquals($exam->get_Qrcode_data(1,1),$data);
     }
  }
  ?>
