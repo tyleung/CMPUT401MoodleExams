a3358c29fb9f56518c2c4484effd2c879d79d31c
jlx339 <jlx339@hotmail.com>
2016-03-29 19:36:07 -0600

    IN process

--- local/memplugin/adrawpdftest.php
+++ local/memplugin/adrawpdftest.php
@@ -51,47 +51,164 @@ function display_draw($js) {
 	//echo '<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>';
 	echo $OUTPUT->header();
 	echo '<script trype="text/javascript" src="js/marktools.js"></script>';
+	// using http://www.w3schools.com/css/css_navbar.asp
 	echo '
 			<style>
+			html, body {
+				width : 100%;
+				height: 100%;
+			}
+
+			#toolbardiv {
+				position:absolute;
+				left:13%;
+				top:10%
+
+			}
+
 			ul.toolbar {
 				list-style-type: none;
 				margin: 0;
 				padding: 0;
-				overflow: hidden;
-				background-color: #333;
+				width: 50px;
+				background-color: #f1f1f1;
 			}
-			li.toolbaritem {
-				float: left;
-			}
+
 			li.toolbaritem a {
 				display: block;
-				color: white;
-				text-align: center;
-				padding: 14px 16px;
+				color: #000;
+				padding: 8px 0 8px 8px;
 				text-decoration: none;
 			}
+
+			/* Change the link color on hover */
 			li.toolbaritem a:hover {
-				background-color: #111;
+				background-color: #555;
+				color: white;
 			}
 			.active {
     			background-color: #4CAF50;
 			}
+
+			#drawcanvas {
+				position:absolute;
+				left: 18%;
+				top:10%;
+			}
+
+			#navigationbardiv {
+				position:absolute;;
+				right:20%;
+				top:10%;
+			}
+
+			ul.navigationbar {
+				list-style-type: none;
+				margin: 0;
+				padding: 0;
+				overflow: hidden;
+				background-color: #f1f1f1;
+			}
+
+			li.navigationarrowup {
+				float:left;
+			}
+
+			li.navigationarrowup a {
+				display: block;
+				color: #000;
+				padding: 8px 8px;
+				text-decoration: none;
+				transform: rotate(180deg);
+			}
+
+			/* Change the link color on hover */
+			li.navigationarrowup a:hover {
+				background-color: #555;
+				color: white;
+			}
+
+			li.navigationarrowleft {
+				float:left;
+			}
+
+			li.navigationarrowleft a {
+				display: block;
+				color: #000;
+				padding: 8px 8px;
+				text-decoration: none;
+				transform: rotate(90deg);
+			}
+
+			/* Change the link color on hover */
+			li.navigationarrowleft a:hover {
+				background-color: #555;
+				color: white;
+			}
+
+			li.navigationarrowright {
+				float:left;
+			}
+
+			li.navigationarrowright a {
+				display: block;
+				color: #000;
+				padding: 8px 8px;
+				text-decoration: none;
+				transform: rotate(270deg);
+			}
+
+			/* Change the link color on hover */
+			li.navigationarrowright a:hover {
+				background-color: #555;
+				color: white;
+			}
+
+			li.navigationarrowdown {
+				float:left;
+			}
+
+			li.navigationarrowdown a {
+				display: block;
+				color: #000;
+				padding: 8px 8px;
+				text-decoration: none;
+			}
+
+			/* Change the link color on hover */
+			li.navigationarrowdown a:hover {
+				background-color: #555;
+				color: white;
+			}
+
+
+
 			</style>
 
-			<div id="markmenu">
-			  <div id="toolbardiv">
+
+			<div id="toolbardiv">
 				<ul class="toolbar">
-				  <li class="toolbaritem"><a><img src = "pix/freehandicon.png" width="30" height="30"></a></li>
-				  <li class="toolbaritem"><a><img src = "pix/stamps/tick.png" width="30" height="30"></a></li>
-				  <li class="toolbaritem"><a><img src = "pix/stamps/cross.png" width="30" height="30"></a></li>
-				  <li class="toolbaritem"><a><img src = "pix/eraseicon.png" width="30" height="30"></a></li>
-				  <li class="toolbaritem"><a><img src = "pix/commenticon.png" width="30" height="30"></a></li>
+					<li class="toolbaritem"><a><img src = "pix/freehandicon.png" width="30" height="30"></a></li>
+					<li class="toolbaritem"><a><img src = "pix/stamps/tick.png" width="30" height="30"></a></li>
+					<li class="toolbaritem"><a><img src = "pix/stamps/cross.png" width="30" height="30"></a></li>
+					<li class="toolbaritem"><a><img src = "pix/eraseicon.png" width="30" height="30"></a></li>
+					<li class="toolbaritem"><a><img src = "pix/commenticon.png" width="30" height="30"></a></li>
 				</ul>
-			  </div>
 			</div>
+			<div id="drawcanvas">
+				<canvas id="canvas" width="500" height="300" ></canvas>
+			</div>
+			<div id="navigationbardiv">
+				<ul class="navigationbar">
+					<li class="navigationarrowup"><a><img src="pix/menu-button-arrow.png" width="30" height="30"></a></li>
+					<li class="navigationarrowleft"><a><img src="pix/menu-button-arrow.png" width="30" height="30"></a></li>
+					<li class="navigationarrowright"><a><img src="pix/menu-button-arrow.png" width="30" height="30"></a></li>
+					<li class="navigationarrowdown"><a><img src="pix/menu-button-arrow.png" width="30" height="30"></a></li>
+				</ul>
+			</div>
+
 			';
-	echo '<script type="text/javascript" src="js/draw.js"></script>
-    	<canvas id="canvas" width="500" height="300" ></canvas>';
+	echo '<script type="text/javascript" src="js/draw.js"></script>';
 	echo $js;
 	echo $OUTPUT->footer();
 }
