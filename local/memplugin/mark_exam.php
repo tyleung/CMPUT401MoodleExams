757910a771853a1b403a91f1c47ee5d65f9bbf44
Anon <jaeyoon1@ualberta.ca>
2016-04-01 05:56:38 -0600

    mark_exam

--- local/memplugin/mark_exam.php
+++ local/memplugin/mark_exam.php
@@ -39,8 +39,6 @@ $PAGE->set_pagelayout('standard');
 $PAGE->set_title(get_string('pluginname', 'local_memplugin'));
 $PAGE->set_heading(get_string('markheader', 'local_memplugin'));
 $PAGE->set_url($CFG->wwwroot.'/local/memplugin/mark_exam.php');
-$node = $PAGE->navigation->add(get_string('markheader', 'local_memplugin'), new moodle_url('mark_exam.php'));
-$node->make_active();
 
 $form = new create_mark_exam_instance();
 
@@ -59,23 +57,35 @@ if($_POST['markbutton']){
 		}
 	}
 	$courses = serialize($choices);
-	var_dump($choices);
+	//var_dump($choices);
 	
 	$exam_data = $form->get_file_content('userfile');
-	$scan = new MME_exam_submission($exam_data);
-
-	//output test
+	
+	#
 	/*
+	$scan = new MME_exam_submission($exam_data);
 	for ($i = 0;$i<3;$i++){
-		echo $scan->get_deserialized_data()[$i]; 
+		echo $scan->get_deserialized_data()[$i].'</br>'; 
 	}
 	*/
 
-	// Do database stuff with exam_submission class.
-
 	redirect($CFG->wwwroot.'/local/memplugin/assign_books.php?courses_ids='.$courses);
 
-} else { 
+} elseif($_POST['savebutton']){
+	$data = $form->get_data();
+
+	$selections = $data->courseboxes;
+	$choices = array();
+
+	foreach($selections as $key => $value){
+		if(strcasecmp($value, '1')==0){
+			$choices[$key] = $value;
+		}
+	}
+	$courses = serialize($choices);
+	redirect($CFG->wwwroot.'/local/memplugin/memhome.php?courses_ids='.$courses);
+}
+else { 
 	if($form->is_cancelled()) {
 		redirect($CFG->wwwroot.'/local/memplugin/memhome.php');
 	} elseif ($data = $form->get_data()) {
