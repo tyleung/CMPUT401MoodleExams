6f6c71eb69c5cfa138ec988d01cd5f927c5691b4
jlx339 <jlx339@hotmail.com>
2016-04-01 01:20:25 -0600

    deleted some usless files

--- config.php
+++ config.php
@@ -1,16 +1,12 @@
 <?php  // Moodle configuration file
-
 unset($CFG);
 global $CFG;
 $CFG = new stdClass();
-
 $CFG->phpunit_prefix = 'phpu_';
 $CFG->phpunit_dataroot = '/var/moodletestdata'; // I HAVE NO IDEA WHAT TO DO
-
 $CFG->behat_prefix = 'b_';
 $CFG->behat_dataroot = '/var/behatdata';
 $CFG->behat_wwwroot = 'http://199.116.235.48/behat';
-
 $CFG->dbtype    = 'mysqli';
 $CFG->dblibrary = 'native';
 $CFG->dbhost    = 'localhost';
@@ -18,25 +14,15 @@ $CFG->dbname    = 'moodledb';
 $CFG->dbuser    = 'moodleuser';
 $CFG->dbpass    = 'moodlepassword';
 $CFG->prefix    = 'mdl_';
-
 $CFG->dboptions = array (
   'dbpersist' => 0,
   'dbport' => 3306,
   'dbsocket' => '',
 );
-
-<<<<<<< HEAD
-
 $CFG->wwwroot   =  'http://192.168.56.102/moodle';
-=======
-$CFG->wwwroot   =  'http://192.168.56.101/moodle';
->>>>>>> 35e3d04f55d33d1505cdbcb98c8434609887097f
 $CFG->dataroot  = '/var/moodledata';
 $CFG->admin     = 'admin';
-
 $CFG->directorypermissions = 0777;
-
 require_once(dirname(__FILE__) . '/lib/setup.php');
-
 // There is no php closing tag in this file,
 // it is intentional because it prevents trailing whitespace problems!
