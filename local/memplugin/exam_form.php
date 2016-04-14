fea420cfeaf9e48201dc557f14a8dfc74c781244
evhill <ehill@ualberta.ca>
2016-04-14 16:37:43 -0600

    Update exam_form.php

--- local/memplugin/exam_form.php
+++ local/memplugin/exam_form.php
@@ -20,7 +20,7 @@
  *
  * @package     local
  * @subpackage  demo_plug-in
- * @copyright   Eric Cheng ec10@ualberta.ca
+ * @copyright   Elyse Hill ehill@ualberta.ca
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
