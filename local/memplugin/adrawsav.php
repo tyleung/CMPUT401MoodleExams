4fa10992e68dcda73ade931bc5d5c8d3caa64ee0
Ryan Satyabrata <kobitoko@users.noreply.github.com>
2016-04-14 16:48:45 -0600

    Update adrawsav.php

--- local/memplugin/adrawsav.php
+++ local/memplugin/adrawsav.php
@@ -1,4 +1,5 @@
 <?php
+
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -15,11 +16,10 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Grid view for an exam.
- *
+ * 
  * @package     local
  * @subpackage  memplugin
- * @copyright   
+ * @copyright   Ryan Satyabrata satyabra@ualberta.ca
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
